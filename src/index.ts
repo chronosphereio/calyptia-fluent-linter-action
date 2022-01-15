patrick-stephens/fluent-linter-action

adding fluent-d support

src/index.ts
@@ -3,9 +3,11 @@ import * as glob from '@actions/glob';
 import { readContent } from './utils/readContent';
 import { FluentBitSchema } from '@calyptia/fluent-bit-config-parser';
 import fetch from 'node-fetch';
-import { CALYPTIA_API_ENDPOINT, CALYPTIA_API_VALIDATION_PATH, PROBLEM_MATCHER_FILE_LOCATION } from './utils/constants';
+import { AGENT_TYPE, CALYPTIA_API_ENDPOINT, CALYPTIA_API_VALIDATION_PATH, PROBLEM_MATCHER_FILE_LOCATION } from './utils/constants';
 import { Annotation, FieldErrors, normalizeErrors } from './utils/normalizeErrors';
 import { formatErrorsPerFile } from './formatErrorsPerFile';
+import { getAgentType } from './utils/getAgentType';
+import { isFluentD } from './utils/isFluentD';
 export enum InputValues {
   CONFIG_LOCATION_GLOB = 'CONFIG_LOCATION_GLOB',
   CALYPTIA_API_KEY = 'CALYPTIA_API_KEY',
@@ -32,10 +34,11 @@ export const main = async (): Promise<void> => {
 
     const content = await readContent(filePath);
 
-    if (FluentBitSchema.isFluentBitConfiguration(content)) {
-      debug(`File ${filePath} seems to be fluent-bit config, validating...`);
+    const agentType = getAgentType(content);
 
-      const URL = `${CALYPTIA_API_ENDPOINT}/${CALYPTIA_API_VALIDATION_PATH}`;
+    if (agentType) {
+      debug(`File ${filePath} seems to be ${agentType} config, validating...`);
+
 
       const headers = {
         'Content-Type': 'application/json',
@@ -43,10 +46,18 @@ export const main = async (): Promise<void> => {
       };
 
       try {
-        const config = new FluentBitSchema(content);
+        let body = JSON.stringify(content);
+
+        if (agentType === AGENT_TYPE.FLUENT_BIT) {
+          const config = new FluentBitSchema(content);
+          body = JSON.stringify(config.schema);
+        }
+
+        const URL = `${CALYPTIA_API_ENDPOINT}/${CALYPTIA_API_VALIDATION_PATH}/${agentType}`;
+
         const response = (await fetch(URL, {
           method: 'POST',
-          body: JSON.stringify(config.schema),
+          body,
           headers,
         })) as Response;
 
@@ -56,7 +67,7 @@ export const main = async (): Promise<void> => {
           debug(`[${filePath}]: ${JSON.stringify(data)}`);
 
           if (data.errors) {
-            const errors = normalizeErrors(filePath, data.errors);
+            const errors = normalizeErrors(filePath, agentType, data.errors);
 
             debug(`${filePath}, Found errors: ${JSON.stringify(errors, null, 2)}`);
             annotations = [...annotations, ...errors];

chronosphereio/calyptia-fluent-linter-action

src/index.ts
@@ -3,9 +3,13 @@ import * as glob from '@actions/glob';
 import { readContent } from './utils/readContent';
 import { FluentBitSchema } from '@calyptia/fluent-bit-config-parser';
 import fetch from 'node-fetch';
-import { CALYPTIA_API_ENDPOINT, CALYPTIA_API_VALIDATION_PATH, PROBLEM_MATCHER_FILE_NAME } from './utils/constants';
+
+import { AGENT_TYPE, CALYPTIA_API_ENDPOINT, CALYPTIA_API_VALIDATION_PATH, PROBLEM_MATCHER_FILE_NAME } from './utils/constants';
 import { Annotation, FieldErrors, normalizeErrors } from './utils/normalizeErrors';
 import { formatErrorsPerFile } from './formatErrorsPerFile';
+import { getAgentType } from './utils/getAgentType';
+
+import { isFluentD } from './utils/isFluentD';
 import { resolve } from 'path';
 export enum InputValues {
   CONFIG_LOCATION_GLOB = 'CONFIG_LOCATION_GLOB',
@@ -33,10 +37,11 @@ export const main = async (): Promise<void> => {
 
     const content = await readContent(filePath);
 
-    if (FluentBitSchema.isFluentBitConfiguration(content)) {
-      debug(`File ${filePath} seems to be fluent-bit config, validating...`);
+    const agentType = getAgentType(content);
 
-      const URL = `${CALYPTIA_API_ENDPOINT}/${CALYPTIA_API_VALIDATION_PATH}`;
+    if (agentType) {
+      debug(`File ${filePath} seems to be ${agentType} config, validating...`);
+
 
       const headers = {
         'Content-Type': 'application/json',
@@ -44,10 +49,18 @@ export const main = async (): Promise<void> => {
       };
 
       try {
-        const config = new FluentBitSchema(content);
+        let body = JSON.stringify(content);
+
+        if (agentType === AGENT_TYPE.FLUENT_BIT) {
+          const config = new FluentBitSchema(content);
+          body = JSON.stringify(config.schema);
+        }
+
+        const URL = `${CALYPTIA_API_ENDPOINT}/${CALYPTIA_API_VALIDATION_PATH}/${agentType}`;
+
         const response = (await fetch(URL, {
           method: 'POST',
-          body: JSON.stringify(config.schema),
+          body,
           headers,
         })) as Response;
 
@@ -57,7 +70,7 @@ export const main = async (): Promise<void> => {
           debug(`[${filePath}]: ${JSON.stringify(data)}`);
 
           if (data.errors) {
-            const errors = normalizeErrors(filePath, data.errors);
+            const errors = normalizeErrors(filePath, agentType, data.errors);
 
             debug(`${filePath}, Found errors: ${JSON.stringify(errors, null, 2)}`);
             annotations = [...annotations, ...errors];
@@ -73,6 +86,7 @@ export const main = async (): Promise<void> => {
 
   if (annotations.length) {
     const location = resolve(__dirname, PROBLEM_MATCHER_FILE_NAME);
+
     console.log(`::add-matcher::${location}`);
 
     const groupedByFile = annotations.reduce((memo, { filePath, errorGroups }) => {
@@ -80,6 +94,7 @@ export const main = async (): Promise<void> => {
 
       return memo;
     }, {} as Record<string, unknown[]>);
+
     for (const file in groupedByFile) {
       console.log(formatErrorsPerFile(file, groupedByFile[file] as Annotation['errorGroups']));
     }